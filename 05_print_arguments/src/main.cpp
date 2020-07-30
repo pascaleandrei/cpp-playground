b42cea70886f83a68139079e2fa3e3c23b6b11d1
Andrei Pascale <pascaleandrei99@gmail.com>
2020-07-30 23:37:33 +0300

    Print arguments received from command line

--- 05_print_arguments/src/main.cpp
+++ 05_print_arguments/src/main.cpp
@@ -1,3 +1,12 @@
-﻿int main() {
+﻿#include <iostream>
+
+int main(int argc, char* argv[]) {
+	if (argc <= 1) {
+		std::cout << "No program arguments found." << std::endl;
+		return 1;
+	}
+	for (int index = 1; index < argc; ++index) {
+		std::cout << argv[index] << std::endl;
+	}
 	return 0;
 }
