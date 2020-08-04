7c49eb86bb8c5ab3ce7f5d830b4cfc614197bd72
Andrei Pascale <pascaleandrei99@gmail.com>
2020-08-05 02:48:54 +0300

    Complete program 07_matrix_to_console

--- 07_matrix_to_console/src/main_matrix_console.cpp
+++ 07_matrix_to_console/src/main_matrix_console.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <iterator>
 
 /**
 	Define a simple matrix.
@@ -10,27 +12,44 @@ class Matrix
 private:
 	size_t column_count;
 	size_t line_count;
+	char** matrix;
 
-	// TODO: store the data
-	// hints: you can use std::string, std::vectors + string, char**, vector<vector<char>>, etc
 public:
-	Matrix(size_t numColumnsX, size_t numLinesY)
-		// TODO: add functionality
+	Matrix(size_t numColumnsX, size_t numLinesY):
+		column_count(numColumnsX),
+		line_count(numLinesY) {
+		matrix = new char* [line_count];
+		for (int index = 0; index < line_count; ++index) {
+			matrix[index] = new char[column_count];
+		}
+	}
+
+	~Matrix()
 	{
-		// TODO: add functionality
+		for (int index = 0; index < line_count; ++index) {
+			delete[] matrix[index];
+		}
+		delete[] matrix;
 	}
 
 	// Set an entire line
 	void setLine(size_t line_number, const std::string& data)
 	{
+		if (data.length() != column_count) {
+			return;
+		}
+		for (int index = 0; index < column_count; ++index) {
+			matrix[line_number][index] = data.at(index);
+		}
 	}
 
-	//OPTIONAL
-	//char getCellXY(size_t x, size_t y, char cell_content)
-	//{
-	//	// TODO: add functionality
-	//	return 0;
-	//}
+	char getCellXY(size_t x, size_t y)
+	{
+		if (x < 0 || x >= column_count || y < 0 || y >= line_count) {
+			return 0;
+		}
+		return matrix[y][x];
+	}
 
 	/**
 		Sets the cell content for a specific cell identified by its coordinates (X and Y)
@@ -54,13 +73,21 @@ public:
 	*/
 	void setCellXY(size_t x, size_t y, char cell_content)
 	{
-		// TODO: add functionality
+		if (x < 0 || x >= column_count || y < 0 || y >= line_count) {
+			return;
+		}
+		matrix[y][x] = cell_content;
 	}
 
 	void print()
 	{
-		// print all lines and columns
-		// TODO: add functionality
+		for (int lineIndex = 0; lineIndex < line_count; ++lineIndex) {
+			for (int columnIndex = 0; columnIndex < column_count; ++columnIndex) {
+				std::cout << matrix[lineIndex][columnIndex];
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
 	}
 };
 
